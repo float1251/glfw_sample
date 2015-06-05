65413a91211c5f689df7048efec6aea1a42560c7
float1251 <taka.red.0502@gmail.com>
2015-06-05 13:47:16 +0900

    gles2対応

--- app.c
+++ app.c
@@ -1,43 +1,89 @@
+#include <GLES2/gl2.h>
 #include <GLFW/glfw3.h>
-#include <OpenGL/gl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("hello world\n");
+void render();
+GLint create_shader(char* src, GLenum type);
+
+static const GLfloat g_vertex_buffer_data[] = {
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+};
+
+int main(void)
+{
     GLFWwindow* window;
 
-    if(!glfwInit())
-    {
+    /* Initialize the library */
+    if (!glfwInit())
         return -1;
-    }
 
-    // http://stackoverflow.com/questions/19658745/why-is-my-opengl-version-always-2-1-on-mac-os-x
-    // これを入れると4.1になる
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);  // yes, 3 and 2!!!
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-
-    if(!window)
+    if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    printf("%s", glGetString(GL_VERSION));
-    glfwSwapInterval(2);
-    
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    while(!glfwWindowShouldClose(window))
+    printf("%s", glGetString(GL_VERSION));
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
     {
+        /* Render here */
+        render();
+
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
+}
+
+void render()
+{
+    GLuint vertexBuffer;
+
+    glClearColor(1.0f, 0, 0, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glGenBuffers(1, &vertexBuffer);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    // 1rst attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glVertexAttribPointer(
+       0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+       3,                  // size
+       GL_FLOAT,           // type
+       GL_FALSE,           // normalized?
+       0,                  // stride
+       (void*)0            // array buffer offset
+    );
+     
+    // Draw the triangle !
+    glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+     
+    glDisableVertexAttribArray(0);
+
 }
